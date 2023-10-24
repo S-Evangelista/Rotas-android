5478b186327ef8d34a0c8e8813c12fc911bfc57a
S-Evangelista <stefanystrenner@gmail.com>
2023-10-24 20:59:33 -0300

    Ajustes finais

--- rotas-android/App.js
+++ rotas-android/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
 import HomeScreen from './src/HomeScreen';
 import UserData from './src/UserData';
 
