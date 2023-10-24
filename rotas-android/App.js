96fafa6f4249f416d6f9b3a7e57e12a6cd471c6f
S-Evangelista <stefanystrenner@gmail.com>
2023-10-24 20:55:41 -0300

    finalizações

--- rotas-android/App.js
+++ rotas-android/App.js
@@ -3,17 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from './src/index.js';
+import HomeScreen from './src/HomeScreen';
+import UserData from './src/UserData';
 
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="UserData" component={UserData} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -24,4 +26,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}
+);
