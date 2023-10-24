597059f362529fd01ad2fbce13006df8e771077b
S-Evangelista <stefanystrenner@gmail.com>
2023-10-24 19:01:33 -0300

    Adicionado importação de aerofunction

--- rotas-android/App.js
+++ rotas-android/App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './src/index.js';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
